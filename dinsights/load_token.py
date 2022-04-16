312670dddc25a70eec367c1fe5793af0fbb431da
cashmere53 <15913755+cashmere53@users.noreply.github.com>
2022-04-16 15:55:13 +0900

    change to convert token file to fullpath

--- dinsights/load_token.py
+++ dinsights/load_token.py
@@ -2,12 +2,21 @@
 
 from pathlib import Path
 
+from loguru import logger
 
-def load_token(filepath: Path) -> str:
+
+def load_token(filepath: str | Path) -> str:
+    if isinstance(filepath, str):
+        filepath = Path(filepath)
+    filepath = filepath.resolve()
+
+    logger.info(f"load token. {filepath=}")
+
     token: str = ""
     with filepath.open("r") as fp:
         token = list(fp)[0]
 
+    logger.info("Success to load token")
     return token
 
 
