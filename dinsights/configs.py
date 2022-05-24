dbec06272758f47a89f398826244e06fade3a0c0
cashmere53 <15913755+cashmere53@users.noreply.github.com>
2022-05-25 00:50:02 +0900

    Configsクラスにデフォルト値を追加

--- dinsights/configs.py
+++ dinsights/configs.py
@@ -6,30 +6,28 @@ from dataclasses import dataclass
 from os import getenv
 from typing import Optional
 
+from loguru import logger
+
 
 @dataclass
 class Configs:
-    devmode: bool
-    talk_channel: str
-
-    @classmethod
-    def default(cls) -> Configs:
-        return cls(
-            devmode=False,
-            talk_channel="game-activities",
-        )
+    devmode: bool = False
+    talk_channel: str = ""
+    verbose: int = 0
 
 
 def configure_from_environments(config: Configs) -> Configs:
     devmode: Optional[str] = getenv("DEVMODE")
-    config.devmode = (devmode is not None) and (devmode.lower() in "yes true 1")
+    logger.debug(f"environment: DEVMODE={devmode}")
+    config.devmode = (devmode is not None) and (devmode.lower() in {"yes", "true", "1"})
 
     talk_channel: Optional[str] = getenv("TALK_CHANNEL")
+    logger.debug(f"environment: TALK_CHANNEL={talk_channel}")
     if talk_channel is not None:
         config.talk_channel = talk_channel
 
     return config
 
 
-configs: Configs = Configs.default()
+configs: Configs = Configs()
 configs = configure_from_environments(configs)
